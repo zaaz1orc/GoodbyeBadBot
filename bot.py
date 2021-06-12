bae2eb549d5ca17176c3691223329ab753005850
Stanislav Kuznecov <la.4pda@gmail.com>
2021-06-12 17:39:30 +0300

    Simple Bot ver. 1

---
+++ bot.py
@@ -0,0 +1,40 @@
+import logging
+import settings
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+	level = logging.INFO,
+	filename = 'bot.log'
+	)
+
+
+def start_bot(update: Updater, context: CallbackContext):
+	print (update)
+	mytext = """Hello {}
+
+	I Have only /start command!  =)""".format(update.message.chat.first_name)
+	update.message.reply_text(mytext)
+
+	# print ("Hello")
+def chat(update: Updater, context: CallbackContext):
+	text = update.message.text
+	logging.info(text)
+
+	update.message.reply_text(text)
+
+
+
+
+def main ():
+	updtr = Updater(settings.TOKEN_TELEGRAMM)
+
+	updtr.dispatcher.add_handler (CommandHandler("start", start_bot))
+	updtr.dispatcher.add_handler (MessageHandler(Filters.text, chat))
+
+
+	updtr.start_polling ()
+	updtr.idle()
+
+if __name__ == "__main__":
+	logging.info('Bot started!')
+	main()
